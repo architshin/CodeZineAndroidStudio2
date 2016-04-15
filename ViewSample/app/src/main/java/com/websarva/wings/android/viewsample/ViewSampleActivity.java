bc64187e234f62cc1da8399a21c6f41c2aa2af98
Shinzo SAITO <architshin@websarva.com>
2016-04-15 12:36:44 +0900

    コメントの不備を修正。

--- ViewSample/app/src/main/java/com/websarva/wings/android/viewsample/ViewSampleActivity.java
+++ ViewSample/app/src/main/java/com/websarva/wings/android/viewsample/ViewSampleActivity.java
@@ -3,6 +3,15 @@ package com.websarva.wings.android.viewsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * CodeZine
+ * Android Studio 2で始めるアプリ開発入門
+ * 画面部品サンプル
+ *
+ * メインアクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
 public class ViewSampleActivity extends AppCompatActivity {
 
 	@Override
