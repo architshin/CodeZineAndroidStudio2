03bfd17613dada6f5a66d741d55239e7426e7aea
Shinzo SAITO <architshin@websarva.com>
2016-04-22 15:43:46 +0900

    リスト選択サンプル追加。

---
+++ ListViewSample/app/src/main/java/com/websarva/wings/android/listviewsample/ListViewSampleActivity.java
@@ -0,0 +1,22 @@
+package com.websarva.wings.android.listviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * CodeZine
+ * Android Studio 2で始めるアプリ開発入門
+ * リスト選択サンプル
+ *
+ * メインアクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
+public class ListViewSampleActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_view_sample);
+	}
+}
