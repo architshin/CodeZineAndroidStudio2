19a4f3fa6b12520ef148103226c01e6fde1c2680
Shinzo SAITO <architshin@websarva.com>
2016-06-15 00:15:22 +0900

    MenuSample追加。

---
+++ MenuSample/app/src/main/java/com/websarva/wings/android/menusample/MenuThanksActivity.java
@@ -0,0 +1,43 @@
+package com.websarva.wings.android.menusample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * CodeZine
+ * Android Studio 2で始めるアプリ開発入門
+ * メニューサンプル
+ *
+ * 注文完了画面のアクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
+public class MenuThanksActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu_thanks);
+
+		Intent intent = getIntent();
+		String menuName = intent.getStringExtra("menuName");
+		String menuPrice = intent.getStringExtra("menuPrice");
+
+		TextView tvMenuName = (TextView) findViewById(R.id.tvMenuName);
+		TextView tvMenuPrice = (TextView) findViewById(R.id.tvMenuPrice);
+
+		tvMenuName.setText(menuName);
+		tvMenuPrice.setText(menuPrice);
+	}
+
+	/**
+	 * 戻るボタンをタップした時の処理。
+	 * @param view 画面部品。
+	 */
+	public void onBackButtonClick(View view) {
+		finish();
+	}
+}
